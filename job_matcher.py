ab3a32081c597cab4aa4b3594f9e2f07d125311d
Ritika <ritikagoyal156@gmail.com>
2026-09-18 13:06:42 +0530

    Connect resume and job description matching

--- job_matcher.py
+++ job_matcher.py
@@ -1,3 +1,19 @@
+from skill_extractor import extract_skills
+from pypdf import PdfReader
+
+def extract_text_from_pdf(pdf_path):
+    reader = PdfReader(pdf_path)
+
+    text = ""
+
+    for page in reader.pages:
+        page_text = page.extract_text()
+
+        if page_text:
+            text += page_text +"\n"
+
+    return text
+
 def calculate_match(resume_skills, job_skills):
     matched_skills = []
     missing_skills = []
@@ -18,6 +34,7 @@ def calculate_match(resume_skills, job_skills):
 
 if __name__ == "__main__":
 
+    """
     resume_skills = [
         "Python",
         "NumPy",
@@ -42,6 +59,49 @@ if __name__ == "__main__":
         resume_skills,
         job_skills
     )
+
+    print("\n--- MATCHED SKILLS ---")
+
+    for skill in matched:
+        print("✓", skill)
+
+    print("\n--- MISSING SKILLS ---")
+
+    for skill in missing:
+        print("✗", skill)
+
+    print(f"\n--- MATCH SCORE: {score:.1f}% ---")
+
+    """
+
+    # read resume
+    resume_text = extract_text_from_pdf("resume.pdf")
+
+    # resume skills
+    resume_skills = extract_skills(resume_text)
+
+    # read the job description
+    with open("job_description.txt","r",encoding="utf-8") as file:
+        job_description = file.read()
+
+    # job description skills
+    job_skills = extract_skills(job_description)
+
+    # compare resume and job
+    matched,missing,score = calculate_match(
+        resume_skills,
+        job_skills
+    )
+
+    print("\n--- RESUME SKILLS ---")
+
+    for skill in resume_skills:
+        print("✓", skill)
+
+    print("\n---JOB SKILLS ---")
+
+    for skill in job_skills:
+        print("-",skill)
 
     print("\n--- MATCHED SKILLS ---")
 
